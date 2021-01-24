fceae3fbe90482a0ddb5b564e67d73877ede8034
shewit <se.shewit.ghiwot@gmail.com>
2021-01-24 03:14:50 -0800

    This is the second commit where variables and functions were declared for the calculator

---
+++ calculator/asset/js/calc.js
@@ -0,0 +1,34 @@
+
+
+var number1;
+var number2;
+var choice;
+let numbers = new Array();
+
+
+choice = prompt("please input the number infront of your service choice \n1. Addition \n2. Subtraction \n3. Multiplication \n" + 
+"4. Division" 
+
+)
+
+function addition(){
+
+};
+
+function subtraction(){
+
+};
+
+function multiplication(){
+
+};
+
+function division(){
+
+};
+
+
+
+
+
+
